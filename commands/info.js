3f6b4a8cbe9a2700b2fb4a1a17f987ecb491050e
George <georgej1088@gmail.com>
2018-11-28 16:35:38 -0500

    Remove useless debug log

--- commands/info.js
+++ commands/info.js
@@ -3,7 +3,6 @@ const {Command} = require('yuuko');
 const {version: erisVersion} = require('../node_modules/eris/package.json');
 const {version: jsdocVersion} = require('../node_modules/jsdoc/package.json');
 const gitCommit = childProcess.execSync('git rev-parse --short HEAD', {encoding: 'utf8'}).slice(0, -1);
-console.log(gitCommit);
 
 module.exports = new Command(['info', 'help', 'version'], msg => {
 	msg.channel.createMessage(`
