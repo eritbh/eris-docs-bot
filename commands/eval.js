9da6cfbba7a4eafeb210231958d677528b24a69e
George <georgej1088@gmail.com>
2018-11-28 16:34:25 -0500

    Add help/info and debug/eval (owner only) commands

---
+++ commands/eval.js
@@ -0,0 +1,71 @@
+const {Command} = require('yuuko');
+const util = require('util');
+
+const inspectOptions = {
+	depth: 1
+};
+
+// We give the prefix and commandName args for debugging purposes
+// eslint-disable-next-line no-unused-vars
+module.exports = new Command(['eval', 'debug', 'js'], (async (msg, args, prefix, commandName) => {
+	// Parse out code blocks
+	args = args.join(' ').replace(/^\s+/, '').replace(/\s*$/, '');
+	if (args.startsWith('```') && args.endsWith('```')) {
+		args = args.substring(3, args.length - 3);
+		if (args.startsWith('js')) {
+			args = args.substr(2);
+		}
+	}
+
+	// Create a dummy console object
+	const c = {
+		_lines: [],
+		_logger (...things) {
+			this._lines.push(...things.join(' ').split('\n'));
+		},
+		_formatLines () {
+			return this._lines.map(line => line && `//> ${line}\n`).join('');
+		}
+	};
+	c.log = c.error = c.warn = c.info = c._logger;
+
+	// Eval the things and send the results
+	let result;
+	try {
+		result = eval(args); // eslint-disable-line no-eval
+	} catch (e) {
+		result = e;
+	}
+	const message = `\`\`\`js\n${c._formatLines()}${util.inspect(result, inspectOptions)}\n\`\`\``;
+
+	// Send the message
+	let outputMsg;
+	try {
+		outputMsg = await msg.channel.createMessage(message);
+	} catch (err) {
+		msg.channel.createMessage(`Error sending message:\n\`\`\`\n${err}\n\`\`\``).catch(() => {});
+		return;
+	}
+
+	// We returned a promise?
+	if (result && typeof result.then === 'function') {
+		// Sweet. Wait for that to resolve.
+		let value;
+		try {
+			value = util.inspect(await result, inspectOptions);
+		} catch (err) {
+			value = err;
+		}
+		// Now we can edit the message with the promise's resolved result(s).
+		const newContent = outputMsg.content.split('\n');
+		newContent.splice(-1, 0, '// Resolved to:', value);
+		try {
+			await outputMsg.edit(newContent.join('\n'));
+		} catch (_) {
+			newContent.splice(-2, 1, '(content too long)');
+			outputMsg.edit(newContent.join('\n')).catch(() => {});
+		}
+	}
+}), {
+	owner: true
+});
---
+++ commands/info.js
@@ -0,0 +1,14 @@
+const childProcess = require('child_process');
+const {Command} = require('yuuko');
+const {version: erisVersion} = require('../node_modules/eris/package.json');
+const {version: jsdocVersion} = require('../node_modules/jsdoc/package.json');
+const gitCommit = childProcess.execSync('git rev-parse --short HEAD', {encoding: 'utf8'}).slice(0, -1);
+console.log(gitCommit);
+
+module.exports = new Command(['info', 'help', 'version'], msg => {
+	msg.channel.createMessage(`
+Usage: \`e;docs ClassName\` or \`e;docs ClassName#methodPropOrEvent\`
+Source: <https://github.com/Geo1088/eris-docs-bot>
+Commit ${gitCommit} jsdoc@${jsdocVersion} eris@${erisVersion}
+	`);
+});
