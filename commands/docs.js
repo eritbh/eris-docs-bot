35edde3e83a037ac4e66969868949eb5c780d5a3
El Jay <ljninjaa@gmail.com>
2018-12-06 20:12:35 +0000

    various improvements & new features

--- commands/docs.js
+++ commands/docs.js
@@ -1,9 +1,11 @@
 const {Command} = require('yuuko');
 
 // Docs data is parsed from jsdoc directly
-const classes = require('../misc/jsdoc_parse');
+const {classes, constants} = require('../misc/jsdoc_parse');
 // Version info, etc. can be found in package.json
-const erisPackage = require('../node_modules/eris/package.json');
+const erisPackage = require('../node_modules/eris/package');
+// All the member categories for class objects
+const memberCategories = ['properties', 'methods', 'events'];
 
 // Properties pulled into all embeds for consistency
 const embedDefaults = {
@@ -15,83 +17,148 @@ const embedDefaults = {
 
 module.exports = new Command(['docs', ''], (msg, args) => {
 	let [classname, search] = args.join(' ').split(/[#. ]/);
-	// 'new Client' => 'Client'
-	if (classname === 'new') {
-		classname = search;
-		search = undefined;
-	}
-
 	if (!classname) {
-		// TODO: class list
-		msg.channel.createMessage('Full Docs: <https://abal.moe/Eris/docs>\nUsage: `e;docs ClassName.methodName`\nthis will generate a class list soon:tm:');
-		return;
+		return msg.channel.createMessage({
+			content: 'Docs: https://abal.moe/Eris/docs',
+			embed: {
+				...embedDefaults,
+				description: classes.map(cls => `[${cls.name}](${linkTo(cls.name)})`).join(', '),
+				title: 'Classes'
+			}
+		}).catch(console.error);
+	} else if (classname.toLowerCase().startsWith('constants')) {
+		if (!search) {
+			return msg.channel.createMessage({
+				content: 'Docs: https://abal.moe/Eris/docs/reference',
+				embed: {
+					...embedDefaults,
+					description: `\`${constants.map(constant => constant.name).join('`, `')}\``,
+					title: 'Constants'
+				}
+			}).catch(console.error);
+		}
+
+		const docsItem = constants.find(constant => constant.name.toLowerCase() === search.toLowerCase());
+		if (docsItem) {
+			return msg.channel.createMessage(formatConstant(docsItem)).catch(console.error);
+		}
+		return msg.channel.createMessage({
+			embed: {
+				...embedDefaults,
+				description: `Couldn't find constant ${classname}.`
+			}
+		}).catch(console.error);
 	}
 	const docsClass = classes.find(cls => cls.name.toLowerCase() === classname.toLowerCase());
 	if (!docsClass) {
-		msg.channel.createMessage(`Couldn't find class ${classname}`).catch(pass);
-		return;
+		let items = constants.find(constant => constant.name.toLowerCase() === classname.toLowerCase());
+		if (items) {
+			return msg.channel.createMessage(formatConstant(items)).catch(console.error);
+		}
+		items = [];
+		for (const cls of classes) {
+			for (const category of memberCategories) {
+				const item = cls[category] && cls[category].find(i => i.name.toLowerCase() === classname.toLowerCase());
+				if (item) items.push({cls, item}); // push to the list of matches
+			}
+		}
+		if (items.length) {
+			return msg.channel.createMessage(handleResults(items)).catch(console.error);
+		}
+		return msg.channel.createMessage({
+			embed: {
+				...embedDefaults,
+				description: `Couldn't find anything matching '${classname}'.`
+			}
+		}).catch(console.error);
+	} else if (!search) {
+		return msg.channel.createMessage(formatClass(docsClass));
 	}
-	if (!search || search === 'constructor') {
-		msg.channel.createMessage(formatClass(docsClass));
-		return;
+	const items = [];
+	for (const category of memberCategories) {
+		const item = docsClass[category] && docsClass[category].find(item => item.name.toLowerCase() === search.toLowerCase());
+		if (item) items.push({cls: docsClass, item}); // push to the list of matches
 	}
-
-	let docsItem;
-	for (const category of ['properties', 'methods', 'events']) {
-		docsItem = docsClass[category] && docsClass[category].find(item => item.name.toLowerCase() === search.toLowerCase());
-		if (docsItem) break; // otherwise try the next set of members
+	if (!items.length) {
+		return msg.channel.createMessage({
+			embed: {
+				...embedDefaults,
+				description: `Couldn't find '${search}' on class ${docsClass.name}`
+			}
+		}).catch(console.error);
 	}
-	if (!docsItem) {
-		msg.channel.createMessage(`Couldn't find ${search} on class ${docsClass.name}`).catch(pass);
-		return;
-	}
-	let contents;
-	// Properties don't have a .kind, but events and methods do
-	if (docsItem.kind) {
-		contents = formatMethodOrEvent(docsItem);
-	} else {
-		contents = formatProperty(docsClass.name, docsItem);
-	}
-	msg.channel.createMessage(contents).catch(pass);
+	msg.channel.createMessage(handleResults(items)).catch(console.error);
 });
 
 // Generate a link to a certain member of a class
-function linkTo (classname, doclet) {
-	if (doclet && doclet.kind) {
-		return `https://abal.moe/Eris/docs/${classname}#${doclet.kind}-${doclet.name}`;
+function linkTo (classname, doclet, kind) {
+	if (doclet) {
+		const memberType = doclet.kind || kind;
+		if (memberType) {
+			return `https://abal.moe/Eris/docs/${classname}#${memberType}-${doclet.name}`;
+		}
 	}
 	return `https://abal.moe/Eris/docs/${classname}`;
 }
 
 // Generate a list of parameters to put in a field value
 function paramList (params, url) {
-	// Exclude object parameter properties
-	const baseParams = params.filter(param => !param.name.includes('.'));
-	let string = baseParams
+	let string = params
 		.map(param => `**\`${param.name}\`** (${param.type})\n${param.description}${param.defaultvalue ? `\nDefault: \`${param.defaultValue}\`` : ''}`)
 		.join('\n\n');
-	if (url && params.length !== baseParams.length) {
+	if (params.some(param => param.name === 'options' && param.type === 'Object')) {
 		string += `\n\nCheck the [full documentation](${url}) for properties on passed objects.`;
-	}
-	// Quick and dirty fit to the length requirement - this happens listing the
-	// client's constructor parameters, for instance
-	// FIXME: It's not desirable that the "more info" text above is cut off;
-	//        maybe it should be added afterwards? Too lazy to worry about it
-	//        for right now
-	if (string.length > 1024) {
-		string = `${string.slice(0, 1021)}...`;
 	}
 	return string;
 }
 
+// Handle multiple results
+function handleResults (results) {
+	if (results.length === 1) {
+		return formatCategory(results[0].cls, results[0].item);
+	} else if (results.length <= 6) {
+		return {
+			content: 'Docs: https://abal.moe/Eris/docs',
+			embed: {
+				...embedDefaults,
+				description: results.map(result => {
+					if (result.item.kind) {
+						return `[\`${result.item.display}\`](${linkTo(result.cls.name, result.item)})`;
+					}
+					return `[\`${result.cls.name}#${result.item.name}\`](${linkTo(result.cls.name, result.item, 'property')})`;
+				}).join(', '),
+				title: 'Multiple Results Found'
+			}
+		};
+	}
+	return {
+		content: 'Docs: https://abal.moe/Eris/docs',
+		embed: {
+			...embedDefaults,
+			description: `\`${results.slice(0, 6).map(result => result.item.display).join('`, `')}\`, and ${results.length - 6} more.`,
+			title: 'Multiple Results Found'
+		}
+	};
+}
+
 // Message formatting functions
+function formatConstant (constant) {
+	return {
+		content: 'Docs: https://abal.moe/Eris/docs/reference',
+		embed: {
+			...embedDefaults,
+			description: constant.value,
+			title: constant.display
+		}
+	};
+}
 function formatClass (docsClass) {
 	const url = linkTo(docsClass.name);
 	return {
 		content: `Docs: <${url}>`,
 		embed: {
 			...embedDefaults,
-			title: `\`${docsClass.name}\``,
+			title: docsClass.name,
 			url,
 			description: docsClass.description,
 			fields: [
@@ -99,7 +166,7 @@ function formatClass (docsClass) {
 					name: 'Constructor Params',
 					value: paramList(docsClass.params, url)
 				},
-				...['properties', 'methods', 'events'].map(category => {
+				...memberCategories.map(category => {
 					const categoryData = docsClass[category];
 					return categoryData && {
 						name: `${categoryData.length} ${category}`,
@@ -111,8 +178,14 @@ function formatClass (docsClass) {
 		}
 	};
 }
+function formatCategory (docsClass, docsItem) {
+	// Properties don't have a .kind, but events and methods do
+	if (docsItem.kind) {
+		return formatMethodOrEvent(docsItem);
+	}
+	return formatProperty(docsClass.name, docsItem);
+}
 function formatMethodOrEvent (doclet) {
-	console.log('Method or event', doclet.display);
 	const classname = doclet.display.replace(/#.*/, '');
 	const url = linkTo(classname, doclet);
 	const message = {
@@ -137,7 +210,7 @@ function formatMethodOrEvent (doclet) {
 	return message;
 }
 function formatProperty (classname, property) {
-	const url = linkTo(classname);
+	const url = linkTo(classname, property, 'property');
 	return {
 		content: `Docs: <${url}>`,
 		embed: {
@@ -153,9 +226,4 @@ function formatProperty (classname, property) {
 			].filter(f => f && f.value)
 		}
 	};
-}
-
-// Readability - .catch(pass) instead of .catch(() => {}) or something else
-function pass (err) {
-	console.error(err);
 }
