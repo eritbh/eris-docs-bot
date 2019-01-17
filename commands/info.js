affdfda3ca267ef974ad539f73c867dfdef3bbef
George <georgej1088@gmail.com>
2019-01-16 22:25:58 -0500

    Allow more common versions of the help command

--- commands/info.js
+++ commands/info.js
@@ -4,7 +4,13 @@ const {version: erisVersion} = require('../node_modules/eris/package');
 const {version: jsdocVersion} = require('../node_modules/jsdoc/package');
 const gitCommit = childProcess.execSync('git rev-parse --short HEAD', {encoding: 'utf8'}).slice(0, -1);
 
-module.exports = new Command(['info', 'help', 'version'], msg => {
+module.exports = new Command([
+	'info', 
+	'help',
+	'version',
+	'ping',
+	'about',
+], msg => {
 	msg.channel.createMessage(`
 Usage: \`e;docs ClassName\` or \`e;docs ClassName#methodPropOrEvent\`
 Source: <https://github.com/Geo1088/eris-docs-bot>
