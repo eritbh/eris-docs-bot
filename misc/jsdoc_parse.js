9edfe15d96f63909a4434f545f2a51553fc00a86
George <georgej1088@gmail.com>
2019-11-13 18:46:21 -0500

    allow huge buffer output from docs parser

--- misc/jsdoc_parse.js
+++ misc/jsdoc_parse.js
@@ -4,7 +4,7 @@ const erisPackage = require('../node_modules/eris/package');
 
 // Execute jsdoc, get the JSON output, parse it, and store it.
 const rawDocsData = JSON.parse(childProcess
-	.execSync('npm run jsdoc', {encoding: 'utf8'})
+	.execSync('npm run jsdoc', {encoding: 'utf8', maxBuffer: Infinity})
 	.replace(/^> [^\n]*/gm, m => console.log(m) || ''));
 console.log();
 // We'll clean up the info and write what we want here
