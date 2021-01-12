20e9bc1a0734ba227aceeedaea991bb9f74c0109
Geo <georgej1088@gmail.com>
2021-01-12 13:32:04 -0500

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -1,6 +1,6 @@
 const {Command} = require('yuuko');
-module.exports = new Command('update', msg => {
-	msg.channel.createMessage('Updating the bot to latest Eris version, gimme a minute or so').catch(() => {});
+module.exports = new Command('update', async msg => {
+	await msg.channel.createMessage('Updating the bot to latest Eris version, gimme a minute or so').catch(() => {});
 	// this will restart the process
 	require('child_process').execSync('npm i --no-save eris@latest; sudo systemctl restart eris-docs-bot.service');
 });
