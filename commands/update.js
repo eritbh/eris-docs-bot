420bf5bb42cc3c5c3227b1a83fd0b0c01f92f945
bsian03 <chharry321@gmail.com>
2021-05-05 19:14:31 +0100

    apply command perm to contribs in Eris server

--- commands/update.js
+++ commands/update.js
@@ -3,4 +3,6 @@ module.exports = new Command('update', async msg => {
 	await msg.channel.createMessage('Updating the bot to latest Eris version, gimme a minute or so').catch(() => {});
 	// this will restart the process
 	require('child_process').execSync('npm i --no-save eris@latest; sudo systemctl restart eris-docs-bot.service');
+}, {
+	custom: msg => msg.member.roles.includes('831967755447828493')
 });
